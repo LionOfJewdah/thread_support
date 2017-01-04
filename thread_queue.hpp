b6ca32a862ab77cf9ec438dda0be52d25d758b55
David Paul Silverstone <dsilvers@usc.edu>
2017-01-04 15:32:02 -0800

    More implementation details for stack and queue

--- thread_queue.hpp
+++ thread_queue.hpp
@@ -4,25 +4,43 @@
 //
 //  Created by David Paul Silverstone on Wed, Jan 4th, 2017.
 //
-//  A thread-safe queue implementation, based on that in
-//  Anthony Williams's "C++ Concurrency in Action"
+//*  A thread-safe queue implementation, based on that in
+//*  Anthony Williams's "C++ Concurrency in Action"
 
 #ifndef thread_queue_hpp
 #define thread_queue_hpp
 
 #include "structs_fwd.hpp"
 #include <deque>
+#include <mutex>
+#include <condition_variable>
+// #include <memory> // std::shared_ptr
 
 namespace david {
     namespace thread {
         template <typename T, class Container = std::deque<T> >
         class thread_queue {
-        private:
         public:
+            using container_type =  Container;
+            using value_type =      T;
+            using reference =       value_type&;
+            using const_reference = const value_type&
+            using size_type =       std::size_t;
+        private:
+            container_type mData;
+            std::mutex mMut;
+            std::condition_variable mCV;
+        public:
+            //* Default constructor. Constructs empty queue.
             thread_queue();
+            //* Copy ctor. Performs deep copy, with lock
             thread_queue(const thread_queue& tq);
+            //* Copy assignment is deleted.
             thread_queue& operator=(const thread_queue& tq) = delete;
+            //* No move assignment or construction.
             ~thread_queue();
+            //* Push by value,
+            void push(T val);
         protected:
 
         };
--- thread_stack.hpp
+++ thread_stack.hpp
@@ -12,12 +12,28 @@
 
 #include "structs_fwd.hpp"
 #include <deque>
+#include <mutex>
+#include <stdexcept>
+//#include <memory> // std::shared_ptr
 
 namespace david {
     namespace thread {
+        struct empty_stack : public std::length::error {
+            static constexpr char err_msg[] = "empty stack.";
+            const char* what() const noexcept { return err_msg; }
+        }
+
         template <typename T, class Container = std::deque<T> >
         class thread_stack {
+        public:
+            using container_type =  Container;
+            using value_type =      T;
+            using reference =       value_type&;
+            using const_reference = const value_type&
+            using size_type =       std::size_t;
         private:
+            std::mutex mMut;
+            container_type mData;
         public:
             thread_stack();
             thread_stack(const thread_stack& ts);
